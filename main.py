2222e91c68d4633773729382a6f7d5b61cbede11
Daave11 <49002232+Daave11@users.noreply.github.com>
2020-07-02 18:21:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 from dht import DHT
 from machine import Pin
 
-TOKEN = "BBFF-askQt4AaljJTSepC03sFIQXptfCZbE" #Put here your TOKEN
+TOKEN = "Token here" #Put here your TOKEN
 DELAY = 60  # Delay in seconds
 
 th = DHT(Pin('P23', mode=Pin.OPEN_DRAIN), 0) # DHT sensor is connected to port 23
@@ -15,7 +15,7 @@ wlan = WLAN(mode=WLAN.STA)
 wlan.antenna(WLAN.INT_ANT)
 
 # Assign your Wi-Fi credentials
-wlan.connect("Vannala 2.4GHz", auth=(WLAN.WPA2, "m202h202i"), timeout=5000)
+wlan.connect("wifi-name", auth=(WLAN.WPA2, "wifi-password"), timeout=5000)
 
 while not wlan.isconnected ():
     machine.idle()
@@ -24,8 +24,6 @@ print("Connected to Wifi\n")
 # Builds the json to send the request
 def build_json(variable1, value1, variable2, value2):
     try:
-        lat = 6.217
-        lng = -75.567
         data = {variable1: {"value": value1},
                 variable2: {"value": value2}}
         return data
